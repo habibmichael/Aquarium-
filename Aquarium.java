4492d05d4a712961935e92dd6d08aad8efebb73a
Michael Habib <Michael Habib>
2016-06-20 21:52:48 -0700

    Add Swim , Draw Methods

--- Aquarium.java
+++ Aquarium.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.lang.Math;
 
 public class Aquarium extends Frame implements Runnable{
     
@@ -63,11 +65,11 @@ public class Aquarium extends Frame implements Runnable{
      
 }
 public void update(Graphics graphics){
-   memoryGraphics.drawImage(aquariumImage,0,0,this)
+   memoryGraphics.drawImage(aquariumImage,0,0,this);
    
    for(int i=0;i<numberFish;i++){
-      ((Fish)fishes.elementAt(i).drawFishImage(memoryGraphics);
-      }
+     ((Fish)fishes.elementAt(i)).drawFishImage(memoryGraphics);      
+     }
       
       graphics.drawImage(memoryImage,0,0,this);
   
@@ -125,6 +127,7 @@ class Fish{
    Image image1;
    Image image2;
    Point location;
+   Point velocity;
    Rectangle edges;
    Random ran;
    
@@ -138,12 +141,67 @@ class Fish{
            this.image2=image2;
            this.edges=edges;
            
-           this.location = new Point(100 + (Math.abs(random.nextInt()) 
-            % 300), 100 + (Math.abs(100 + random.nextInt()) % 100));
+           this.location = new Point(100 + (Math.abs(ran.nextInt()) 
+            % 300), 100 + (Math.abs(100 + ran.nextInt()) % 100));
 
-        this.velocity = new Point(random.nextInt() % 8, 
-            random.nextInt() % 8); 
+        this.velocity = new Point(ran.nextInt() % 8, 
+            ran.nextInt() % 8); 
                }
+               
+       public void swim() 
+    {
+        if(ran.nextInt() % 7 <= 1){
+
+            velocity.x += ran.nextInt() % 4; 
+
+            velocity.x = Math.min(velocity.x, 8);
+            velocity.x = Math.max(velocity.x, -8);
+
+            velocity.y += ran.nextInt() % 4; 
+
+            velocity.y = Math.min(velocity.y, 8);
+            velocity.y = Math.max(velocity.y, -8);
+        }
+    
+        location.x += velocity.x;
+        location.y += velocity.y;
+
+        if (location.x < edges.x) {
+            location.x = edges.x;
+            velocity.x = -velocity.x;
+        }
+
+        if ((location.x + image1.getWidth(tank))
+            > (edges.x + edges.width)){
+            location.x = edges.x + edges.width - 
+                image1.getWidth(tank);
+            velocity.x = -velocity.x;
+        }
+    
+        if (location.y < edges.y){
+            location.y = edges.y;
+            velocity.y = -velocity.y;
+        }
+
+        if ((location.y + image1.getHeight(tank))
+            > (edges.y + edges.height)){
+            location.y = edges.y + edges.height - 
+                image1.getHeight(tank);
+            velocity.y = -velocity.y;
+        }
+    }
+  
+    public void drawFishImage(Graphics g)
+    {
+        if(velocity.x < 0) {
+            g.drawImage(image1, location.x, 
+                location.y, tank);
+        }
+        else {
+            g.drawImage(image2, location.x, 
+                location.y, tank);
+        }
+    }
 
    
 }
